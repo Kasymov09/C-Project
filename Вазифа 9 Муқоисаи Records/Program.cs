97f396caf8c5f4fcb53f2a565440e7532de8c564
Kasymov09 <lolavalijon@gmail.com>
2025-02-17 21:56:59 +0500

    Update Program.cs

--- Вазифа 9 Муқоисаи Records/Program.cs
+++ Вазифа 9 Муқоисаи Records/Program.cs
@@ -3,3 +3,4 @@ var emp1 = new Employee("Ahmad", 30);
 var emp2 = new Employee("Ahmad", 30);
 System.Console.WriteLine(emp1 == emp2);
 public record Employee(string Name, int Age);
+//Hej chiz ivaz nakardam
